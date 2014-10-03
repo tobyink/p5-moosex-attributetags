8fb48cc5523800ce339bbf0d2ced539d3e35be0d
Toby Inkster <mail@tobyinkster.co.uk>
2014-10-03 22:29:59 +0100

    bump version; changelog

--- lib/MooseX/AttributeTags.pm
+++ lib/MooseX/AttributeTags.pm
@@ -5,7 +5,7 @@ use warnings;
 package MooseX::AttributeTags;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use Carp;
 use Data::OptList qw(mkopt);
--- lib/MooseX/AttributeTags/PRole.pm
+++ lib/MooseX/AttributeTags/PRole.pm
@@ -5,7 +5,7 @@ use warnings;
 package MooseX::AttributeTags::PRole;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use MooseX::Role::Parameterized;
 
